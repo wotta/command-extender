8836aa009c26c5c3ec1bbcab7717a37970c81ef6
wouter <wouter.van.marrum@protonmail.com>
2020-03-05 23:27:37 +0100

    Created TestCase.php; So we have one base testcase.

---
+++ tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Wotta\CommandExtender\Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+use Wotta\CommandExtender\CommandExtenderServiceProvider;
+
+class TestCase extends BaseTestCase
+{
+    protected function getPackageProviders($app): array
+    {
+        return [
+            CommandExtenderServiceProvider::class
+        ];
+    }
+}
